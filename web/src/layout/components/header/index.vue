3037c6e7e3d477d2892230212312f0524c3eef8a
xujf9 <xujf9@asiainfo.com>
2021-05-16 09:54:37 +0800

    修改菜单栏

--- web/src/layout/components/header/index.vue
+++ web/src/layout/components/header/index.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <div class="header_l">
       <a href="javascript:;" class="logoLink"
-        ><img src="./logo.png" alt="logo" />Vue Project</a
+        >Menu</a
       >
     </div>
     <div class="header_r">
